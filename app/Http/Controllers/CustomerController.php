5630703108641f874563f28353b5c00cee9f077d
TadasBaltru <tadasbaltru@gmail.com>
2025-05-21 18:01:50 +0300

    Customers crud, with request, modal add/edit form with flash messages

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\CustomerRequest;
 use App\Models\Customer;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Redirect;
 use Inertia\Inertia;
 
 class CustomerController extends Controller
@@ -28,9 +31,14 @@ class CustomerController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(CustomerRequest $customer_request)
     {
-        //
+        Customer::create([
+            ...$customer_request->validated(),
+            'user_id'=>Auth::id()
+
+        ]);
+        return redirect()->back()->with('success', 'Customer created!');
     }
 
     /**
@@ -44,17 +52,19 @@ class CustomerController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(Customer $customer)
+    public function edit(Customer $customer, CustomerRequest $customer_request)
     {
-        //
+
+
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Customer $customer)
+    public function update(CustomerRequest $customer_request, Customer $customer)
     {
-        //
+        $customer->update([...$customer_request->validated()]);
+        return redirect()->route("customers.index")->with('success', 'Customer updated!');
     }
 
     /**
--- resources/js/pages/customers/index.tsx
+++ resources/js/pages/customers/index.tsx
@@ -1,9 +1,11 @@
 import { PlaceholderPattern } from '@/components/ui/placeholder-pattern';
 import AppLayout from '@/layouts/app-layout';
 import { type BreadcrumbItem } from '@/types';
-import { Head, router } from '@inertiajs/react';
+import { Head, router, usePage } from '@inertiajs/react';
 import {Button} from '@/components/ui/button'
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import CustomerModalForm from './CustomerModalForm';
+
 
 const breadcrumbs: BreadcrumbItem[] = [
     {
@@ -32,13 +34,32 @@ export default function CustomersIndex({customers}: Props) {
             router.delete(`/customers/${id}`);
         }
     };
+
+    const [modalOpen, setModalOpen] = useState(false);
+    const [selectedContact, setSelectedCustomer] = useState(null);
+
+    const openModal = (contact = null) => {
+        setSelectedCustomer(contact);
+        setModalOpen(true);
+    };
+    const { props } = usePage();
+    const flash = props.flash;
+
     return (
         <AppLayout breadcrumbs={breadcrumbs}>
+            {flash.success && (
+                <div className="bg-green-100 border border-green-300 text-green-800 px-4 py-2 rounded mb-4">
+                    {flash.success}
+                </div>
+            )}
             <Head title="Customers" />
 
-            <div className="p-6">
-                <h1 className="text-2xl font-semibold mb-4">Customers</h1>
 
+
+            <div className="p-6">
+
+                <h1 className="text-2xl font-semibold mb-4">Customers</h1>
+                <Button onClick={() => openModal()} variant={"create"}>Create new Customer</Button>
                 <div className="overflow-x-auto bg-white shadow rounded-lg">
                     <table className="min-w-full divide-y divide-gray-200">
                         <thead className="bg-gray-100">
@@ -59,7 +80,7 @@ export default function CustomersIndex({customers}: Props) {
                                 <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{customer.phone}</td>
                                 <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{customer.created_at}</td>
                                 <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500"><Button
-                                    variant={"outline"}>Update</Button>
+                                    variant={"outline"} onClick={() => openModal(customer)} >Update</Button>
                                 </td>
                                 <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500"><Button onClick={() => handleDelete(customer.id)} variant={"destructive"}>Delete</Button>
                                 </td>
@@ -69,6 +90,15 @@ export default function CustomersIndex({customers}: Props) {
                     </table>
                 </div>
             </div>
+            {modalOpen && (
+                <CustomerModalForm
+                    customer={selectedContact}
+                    onClose={() => {
+                        setModalOpen(false);
+                        setSelectedCustomer(null);
+                    }}
+                />
+            )}
         </AppLayout>
     );
 }
