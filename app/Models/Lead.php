13b20ec6f5477e4d2f10ac40e320cc1d008be011
TadasBaltru <tadasbaltru@gmail.com>
2025-05-22 11:23:21 +0300

    Leads migration, model, seeds, factories, controller init

---
+++ app/Models/Lead.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Lead extends Model
+{
+    /** @use HasFactory<\Database\Factories\LeadFactory> */
+    use HasFactory;
+    protected $fillable = ['user_id','customer_id', 'name', 'email', 'phone','status', 'notes'];
+
+}
